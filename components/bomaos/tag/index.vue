d47369f45690154d47784d4d66f0e77fce44ffd2
Flour_Mo <1283468667@qq.com>
2023-11-03 18:26:30 +0800

    Flour_Mo: 新增自定义Tag组件

---
+++ components/bomaos/tag/index.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const props = defineProps<{
+  // 弹窗是否打开
+  url?: string;
+}>();
+</script>
+
+<template>
+  <nuxt-link class="bomaos-tag" :to="url ?? ''">
+    <div class="icon">
+      <slot name="icon"/>
+    </div>
+    <div class="content">
+      <slot/>
+    </div>
+  </nuxt-link>
+</template>
+
+<style scoped lang="less">
+.bomaos-tag {
+  background-color: #f0f0f0;
+  padding: 5px 10px;
+  border-radius: 100px;
+  display: flex;
+  align-items: center;
+  color: #1c1c1e;
+
+  .icon {
+    display: flex;
+    align-items: center;
+    width: 13px;
+    height: 13px;
+  }
+
+  .content {
+    margin-left: 3px;
+    font-size: 12px;
+  }
+}
+</style>
